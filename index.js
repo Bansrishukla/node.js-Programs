761dca3a17f337730c728fdcb4f2229a70cc8e83
Bansrishukla <bsgeetshukla@gmail.com>
2025-07-14 20:20:17 +0530

    init with basic program

---
+++ index.js
@@ -0,0 +1,14 @@
+// checkEvenOdd.js
+
+// Function to check even or odd
+function checkEvenOrOdd(number) {
+    if (number % 2 === 0) {
+        console.log(`${number} is Even`);
+    } else {
+        console.log(`${number} is Odd`);
+    }
+}
+
+// Example usage
+checkEvenOrOdd(10); // Output: 10 is Even
+checkEvenOrOdd(7);  // Output: 7 is Odd
